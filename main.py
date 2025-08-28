e3c9a56bcce8fe24d6a54f998148a2cbaf52239d
Mata4ari <ffllyfnya@gmail.com>
2025-08-28 13:34:54 +0300

    kej3qwk

--- main.py
+++ main.py
@@ -214,6 +214,7 @@ async def parse_data():
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
+        host='0.0.0.0',
         port=int(os.getenv("PORT") or 8000),
         reload=False
     )
